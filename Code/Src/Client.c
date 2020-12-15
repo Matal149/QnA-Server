9494d81e3d0501e8d4ed323c561437a94fcdfe42
Matal149 <dovahmatt917@gmail.com>
2020-12-15 21:38:33 +0100

    Mise en place de menus dans les terminaux du serveur et du client

--- Code/Src/Client.c
+++ Code/Src/Client.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
 
@@ -18,6 +18,7 @@
 
 int flag = 1;
 
+void printMenuFils(void);
 
 void catchCtrlC()
 {
@@ -25,35 +26,41 @@ void catchCtrlC()
    printf("passe ctrl C\n");
 }
 
-
-void stop(int sig){
-    printf("CLIENT : Arreter par le Serveur \n");
-    exit(0);
+void stop(int sig)
+{
+   printf("CLIENT : Arreter par le Serveur \n");
+   exit(0);
 }
-
 
 void viderBuffer()
 {
-    int c = 0;
-    while (c != '\n' && c != EOF)
-    {
-        c = getchar();
-    }
+   int c = 0;
+   while (c != '\n' && c != EOF)
+   {
+      c = getchar();
+   }
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
    int dp;
+
+   char choixFils;
+
+   int bclFermetureClient = 1; //On déclare une varialbe permlettant de boucler à l'infini tant qu'on a pas effectuer la bonne conbinaison de touches (ctrl+D)
+
    printf("CLIENT : %s\t%d\n", argv[1], getppid());
-   dp=open("pipeInitial",O_WRONLY);
+   dp = open(argv[1], O_WRONLY);
+
+   printf("ouverture PIPE : %s\n", argv[1]);
 
    char buffer[BUFFERSIZE] = {};
-   
+
    signal(SIGINT, catchCtrlC);
 
    //Signal pour voir si on doit stopper le fils
    signal(SIGUSR1, stop);
-
+   /*
    while(flag)
    {
       printf("Entre une chaine : \n");
@@ -63,8 +70,66 @@ int main(int argc, char * argv[])
       write(dp, buffer, strlen(buffer)+1 );
    }
    printf("Fin While\n");
-//close(dp);
-/*
+*/
+
+   while (1) //on boucle à l'infini
+   {
+      printMenuFils();
+      scanf("%c", &choixFils);
+      getchar();
+      switch (choixFils)
+      {
+      case '0':
+         //dp=open(arg[1],O_WRONLY);
+         //strcat(fermeture,Cpid);
+         //write(dp, fermeture,250);
+         close(dp); // On ferme le pipe
+         printf("Fermeture du pipe\n");
+         while (bclFermetureClient == 1)
+         {
+            printf("Appuyer sur CTR+D pour fermer le client\n");
+            char buffer[1];
+
+            if (fgets(buffer, 10, stdin) == NULL)
+            {
+               bclFermetureClient = 0;
+            }
+            else
+            {
+               printf("Saisi la bonne commande pour quitter\nJe t'aide un peu Ctrl+D (^o^)");
+            }
+         }
+
+         printf("Fermeture du fils\n");
+         exit(0);
+         break;
+
+      case '1':
+         printf("Tu veux demander quoi à Akinator du pauvre ? (⌐■_■):\n");
+         //scanf(" %[^\n]",question);
+         scanf("%s", buffer);
+         getchar();
+
+         //strcat(question,"\t");   //Ajout d'un caractere de séparation
+         //strcat(question,Cpid);		// On ajoute le pid a fin de la question
+
+         dp = open(argv[1], O_WRONLY); // Ouverture du PIPE puis ecriture de la question
+         //nbcarecrit = write(dp, question,sizeof(question));
+         write(dp, buffer, strlen(buffer) + 1);
+
+         sleep(1);  // Tempo d'une seconde
+         close(dp); // Fermeture du PIPE
+         break;
+
+      default:
+         printf("J'ai pas compris ce que tu me demandes, entre une commande (magique) correcte\t(ノ°∀°)ノ⌒･*:.｡. .｡.:*･゜ﾟ･*☆\n");
+         break;
+      }
+   }
+   printf("Fin While\n");
+
+   //close(dp);
+   /*
    char buffer[BUFFERSIZE];
     printf("Enter a message: \n");
     while(fgets(buffer, BUFFERSIZE , stdin) != NULL)
@@ -78,6 +143,16 @@ int main(int argc, char * argv[])
         
     }
 */
-   close(dp);
+   //close(dp);
    //exit(0);
+}
+
+void printMenuFils(void)
+{
+   printf("\n\tHello jeune client entrepreneur");
+   printf("\n\nQuestionner le serveur............: 1\n");
+   //printf("Liste des mots clés...........: 2\n");
+   printf("Pour quitter le client.......................: 0\n");
+   printf("Ton choix: ");
+   rewind(stdin); //permet de se positionner au début d'un fichier, ici le flux d'entré
 }
--- Code/Src/Serveur.c
+++ Code/Src/Serveur.c
@@ -5,10 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -17,8 +16,9 @@
 
 #define TAILLE_MAX_CHAINE 100
 
-
 volatile int stopLecture = 1;
+
+void printMenuPere(void);
 
 void stoperLecture()
 {
@@ -26,93 +26,145 @@ void stoperLecture()
 }
 
 //converti la chaine de caracteres en minuscules
-char* toLower(char* s)
+char *toLower(char *s)
 {
-  for(char *p=s; *p; p++) *p=tolower(*p);
-  return s;
+    for (char *p = s; *p; p++)
+        *p = tolower(*p);
+    return s;
 }
 
-
-
-//Vérification par mots-clés et affichage de la réponse
-void traitementChaineRecue(char * chaineRecue)
+////Fonction qui traite les chaines de caractères reçues des clients. Vérification par mots-clés et affichage de la réponse
+void traitementChaineRecue(char *chaineRecue)
 {
     //conversion de la chaine en minuscile pour faciliter le traitement
     //char chaineMinuscule[TAILLE_MAX_CHAINE];
     chaineRecue = toLower(chaineRecue);
 
-    if(strstr(chaineRecue, "oui") != NULL || strstr(chaineRecue, "non") != NULL || strstr(chaineRecue, "ah") != NULL)
+    if (strstr(chaineRecue, "oui") != NULL || strstr(chaineRecue, "non") != NULL || strstr(chaineRecue, "ah") != NULL)
     {
         printf("E Y E S, e-yes\n");
     }
-    else if(strstr(chaineRecue, "yes") != NULL || strstr(chaineRecue, "nope") != NULL || strstr(chaineRecue, "geh") != NULL)
+    else if (strstr(chaineRecue, "yes") != NULL || strstr(chaineRecue, "nope") != NULL || strstr(chaineRecue, "geh") != NULL)
     {
         printf("Y U geh ?\n");
     }
+    else if (strstr(chaineRecue, "intelligent") != NULL || strstr(chaineRecue, "malin") != NULL || strstr(chaineRecue, "smart") != NULL)
+    {
+        printf("Nan mais aussi t'abuses là, on te dis de PAS demander si je suis intelligent ou malin\n");
+        sleep(5);
+        printf("et toi, bien évidement, tu demandes (ノಠ益ಠ)ノ彡┻━┻\n");
+        sleep(3);
+        printf("Pour qui tu me prends ? Google ? (¬_¬)\n");
+        sleep(3);
+        printf("Bon en vrai je peux pas t'en vouloir c'est vrai que c'est tentant (^o^)\n");
+    }
     else
     {
-        // On ne détecte pas de mots-clés 
-        printf("Il nous est impossible pour le moment de vous répondre, l'intelligence de notre serveur n'est pas encore assez développée :(\n");
+        // On ne détecte pas de mots-clés, on affiche un message explicatif
+        printf("Il nous est impossible pour le moment de vous répondre, l'intelligence de notre serveur n'est pas encore assez développée (aucun mot-clé n'a été détecté dans votre question) ¯\\_(ツ)_/¯\n");
     }
-    
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int pid,desc,nb ;
+    int pid, desc, nb;
     char chaineRecue[TAILLE_MAX_CHAINE];
 
-    //Création du pipe initial, 
-    unlink("pipeInitial");
-    mkfifo("pipeInitial" ,0666);
+    char choixPere; //Permet de stocker le choix dans le menu du serveur
+    int nb_clients;
+
+    //Création du pipe initial,
+    unlink(argv[1]);
+    mkfifo(argv[1], 0666);
 
     //int stopLecture = 1;
-    
-    //Association du 
+
+    //Association du
     signal(SIGCHLD, stoperLecture);
 
-    pid=fork();
+    pid = fork();
 
-    if(pid == 0)
-    {
-        printf("\nEXECUTION CLIENT\n" );
-        execl("/usr/bin/xterm" , "xterm", "-e", "./Client", "Client", NULL);
+    if (pid == 0)
+    { //on lance le programme de créatio nde clients
+        printf("\nEXECUTION CLIENT\n");
+        execl("/usr/bin/xterm", "xterm", "-e", "./Client", argv[1], "Client", NULL);
     }
     else
     {
-        desc=open("pipeInitial",O_RDONLY);
-
-    //boucler ici
-        while(stopLecture == 1)
+        while (1)
         {
-            nb=read(desc,chaineRecue,TAILLE_MAX_CHAINE);
-            chaineRecue[nb] = '\0';
-            printf("Message reçu du client : '%s'\n ",chaineRecue);
+            desc = open(argv[1], O_RDONLY);
 
-            //Vérification si chaine reçue contient "fermer" ou "quitter"
-            if(strstr(chaineRecue, "quitter") == NULL)
-            {
-                //traitement des sous chaines suivant mot clés
-                traitementChaineRecue(chaineRecue);
-            }
-            else 
-            {
-                stopLecture = 0;
+            printMenuPere();
+            scanf(" %c", &choixPere);
+            getchar();
 
-                //Arret du Client
-                kill(pid,SIGUSR1);
+            switch (choixPere)
+            {
+            case '1':
+
+                break;
+
+            case '3':
+                printf("Creation d'un nouveau fils\n");
+
+                if (pid > 0)
+                {
+                    pid = fork();
+                    if (pid == 0)
+                    {
+                        //on lance le programme de créatio nde clients
+                        execl("/usr/bin/xterm", "xterm", "-e", "./Client", argv[1], "Client", NULL);
+                    }
+                }
+                printf("Le nouveau client est prêt\n");
+                break;
+
+            case '2':
+                while (stopLecture == 1)
+                {
+                    nb = read(desc, chaineRecue, TAILLE_MAX_CHAINE);
+                    chaineRecue[nb] = '\0';
+                    printf("Message reçu du client : '%s'\n ", chaineRecue);
+
+                    //Vérification si chaine reçue contient "fermer" ou "quitter"
+                    if (strstr(chaineRecue, "quitter") == NULL)
+                    {
+                        //traitement des sous chaines suivant mot clés
+                        traitementChaineRecue(chaineRecue);
+                    }
+                    else
+                    {
+                        stopLecture = 0;
+
+                        //Arret du Client
+                        kill(pid, SIGUSR1);
+                    }
+                    //Arret de la lecture infinie
+                    printf("stopLecture : %d\n", stopLecture);
+                }
+                break;
+
+            default:
+                break;
             }
-                //Arret de la lecture infinie
-                //stopLecture = stopLecture();
-            printf("stopLecture : %d\n", stopLecture);
         }
-        
     }
-//fin boucle        
-        printf("FIN Programme apres boucle StopLecture\n");
-        close(desc);
-        wait(NULL);
+    //fin boucle
+    printf("FIN Programme apres boucle StopLecture\n");
+    close(desc);
+    wait(NULL);
 
-        exit(0);
+    exit(0);
 }
 
+void printMenuPere(void)
+{
+    printf("\n\tBienvenue sur le Akinator du pauvre\n");
+    printf("Tu veux connaitre le nombre de client ?:\t\t\t 1\n");
+    printf("Pour parler avec les clients:\t\t\t 2\n");
+    printf("Tu veux créer un nouveau client ?:\t\t\t 3\n");
+    printf("Pour tout quitter \t\t\t 0\n");
+    printf("Ton choix: ");
+    rewind(stdin); //permet de se positionner au début d'un fichier, ici le flux d'entré
+}
